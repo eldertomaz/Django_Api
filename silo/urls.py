7d15df9ffe355a3cfd620f381acafbc71b1d81a0
Elder <eldertomazinho@gmail.com>
2021-11-01 16:46:06 -0300

    atualização das urls

--- silo/urls.py
+++ silo/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     #POST
-    url(r'^silo_feed_acquisition/', views.SiloList.as_view(), name='silo-list'),
+    url(r'^silo_feed_acquisition/', views.SiloList.as_view({'post': 'list'}), name='silo-list'),
     #GET
-    url(r'^silo_feed_weight/<int:id>', views.SiloList.as_view(), name='silo-list'),
+    url(r'^silo_feed_weight/', views.SiloList.as_view({'get': 'list'}), name='silo-list'),
 
 ]
 
--- silo/views.py
+++ silo/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Silo
 from .serializers import SiloSerializer
 from django.http import HttpResponse
 
 
-class SiloList(generics.ListCreateAPIView):
+class SiloList(viewsets.ModelViewSet):
 
     queryset = Silo.objects.all()
     serializer_class = SiloSerializer
+    # def get_queryset(self):
+    #     return super().get_queryset()
